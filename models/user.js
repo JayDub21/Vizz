119e895234a7e3e6b72e022f62063fa91d3d03c1
JayDub21 <justin.wofford.21@gmail.com>
2020-11-30 16:38:11 -0600

    user schema / route

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const Joi = require('joi-oid');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+       
+      }
+}));
+
+const validateUser = (user) => {
+    // Joi.object and schema.validate is new way to write
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    })
+    return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
