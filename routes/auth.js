6bd82ebde647af6c507568ac6453211618865827
JayDub21 <justin.wofford.21@gmail.com>
2020-12-01 06:28:57 -0600

    basic auth setup

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const Joi = require('joi-oid');
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send('Invalid email or password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password.');
+
+  res.send(true);
+
+  });
+
+  const validate= (req) => {
+    // Joi.object and schema.validate is new way to write
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    })
+    return schema.validate(req);
+}
+
+module.exports = router;
