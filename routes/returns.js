01114328d0fd894d2eac128e2267df43a9df6c73
JayDub21 <justin.wofford.21@gmail.com>
2020-12-09 22:41:42 -0600

    added validation for return routes

--- routes/returns.js
+++ routes/returns.js
@@ -1,3 +1,4 @@
+const Joi = require('joi-oid');
 const moment = require('moment');
 const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
@@ -6,8 +7,9 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", auth, async (req, res) => {
-if(!req.body.customerId) return res.status(400).send('customerId not provided');
-if(!req.body.movieId) return res.status(400).send('movieId not provided');
+const { error } = validateReturn(req.body);
+if (error) return res.status(400).send(error.details[0].message);
+  
 
 const rental = await Rental.findOne({
     'customer._id': req.body.customerId,
@@ -26,8 +28,18 @@ await Movie.update({ _id: rental.movie._id }, {
     $inc: {numberInStock: 1}
 });
 
-return res.status(200).send();
+return res.status(200).send(rental);
 
 });
+
+const validateReturn = (req) => {
+    // Joi.object and schema.validate is new way to write
+    const schema = Joi.object({
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required(),
+    });
+
+    return schema.validate(req)
+};
 
 module.exports = router;
