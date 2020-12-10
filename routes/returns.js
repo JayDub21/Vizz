a67ea5f8aae80eab773f6ad4fb32baf9de3df7f3
JayDub21 <justin.wofford.21@gmail.com>
2020-12-09 23:18:09 -0600

    refactoring

--- routes/returns.js
+++ routes/returns.js
@@ -1,34 +1,29 @@
 const Joi = require('joi-oid');
-const moment = require('moment');
 const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const auth = require('../middleware/auth');
 const express = require("express");
 const router = express.Router();
 
+
 router.post("/", auth, async (req, res) => {
 const { error } = validateReturn(req.body);
 if (error) return res.status(400).send(error.details[0].message);
-  
 
-const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId,
-});
+const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
 if (!rental) return res.status(404).send('Rental not found.');
 
 if (rental.dateReturned) return res.status(400).send('Return already processed');
 
-rental.dateReturned = new Date();
-const rentalDays = moment().diff(rental.dateOut, 'days') ;
-rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+rental.return();
 await rental.save();
 
 await Movie.update({ _id: rental.movie._id }, {
     $inc: {numberInStock: 1}
 });
 
-return res.status(200).send(rental);
+return res.send(rental);
 
 });
 
