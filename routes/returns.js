bfb720d6835f63adc7d70249c2504be99801c1bb
JayDub21 <justin.wofford.21@gmail.com>
2020-12-09 21:11:51 -0600

    added happy test for return

--- routes/returns.js
+++ routes/returns.js
@@ -1,10 +1,11 @@
 const {Rental} = require('../models/rental');
+const auth = require('../middleware/auth');
 const express = require("express");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
- if(!req.body.customerId) return res.status(400).send('customerId not provided');
- if(!req.body.movieId) return res.status(400).send('movieId not provided');
+router.post("/", auth, async (req, res) => {
+if(!req.body.customerId) return res.status(400).send('customerId not provided');
+if(!req.body.movieId) return res.status(400).send('movieId not provided');
 
 const rental = await Rental.findOne({
     'customer._id': req.body.customerId,
@@ -12,7 +13,8 @@ const rental = await Rental.findOne({
 });
 if (!rental) return res.status(404).send('Rental not found.');
 
-res.status(401).send('Unauthorized');
+if (rental.dateReturned) return res.status(400).send('Return already processed');
+return res.status(200).send();
 });
 
 module.exports = router;
--- tests/integration/routes/returns.test.js
+++ tests/integration/routes/returns.test.js
@@ -39,48 +39,55 @@ describe('/api/returns', () => {
     })
     afterEach(async () => { 
         await server.close(); 
-        await Rental.deleteMany({});
+        await Rental.deleteMany({});   
+  });
+
+
+
+    it('should return 401 if client is not logged in',async () => {
+        token = '';
+
+        const res = await exec();
+
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if customerId is not provided',async () => {
+        customerId = '';
+
+        const res = await exec();
         
-  });
-
-  it('should return 404 if no rental found for this customer/movie',async () => {
-    await Rental.deleteMany({});
-
-    const res = await exec();
-
-    expect(res.status).toBe(404);
-
+        expect(res.status).toBe(400);
     });
 
-  it('should return 401 if client is not logged in',async () => {
-    token = '';
+    it('should return 400 if movieId is not provided',async () => {
+        movieId = '';
 
-    const res = await exec();
-
-    expect(res.status).toBe(401);
-
-  });
-
-  it('should return 400 if customerId is not provided',async () => {
-    customerId = '';
-
-    const res = await exec();
-    
-    expect(res.status).toBe(400);
-
+        const res = await exec();
+        
+        expect(res.status).toBe(400);  
     });
 
-it('should return 400 if movieId is not provided',async () => {
-    movieId = '';
+    it('should return 404 if no rental found for this customer/movie',async () => {
+        await Rental.deleteMany({});
 
-    const res = await exec();
-    
-    expect(res.status).toBe(400);
-    
+        const res = await exec();
+
+        expect(res.status).toBe(404);
     });
 
+    it('should return 400 if rental is already processed',async () => {
+        rental.dateReturned = new Date();
+        await rental.save();
 
+        const res = await exec();
 
-  
-    
-})
+        expect(res.status).toBe(400);
+    });  
+
+    it('should return 200 for a valid request',async () => {
+        const res = await exec();
+
+        expect(res.status).toBe(200);
+    });  
+});
