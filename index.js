ea0570f9e815f6da3fcc1f6c1d7e890303c3b998
JayDub21 <justin.wofford.21@gmail.com>
2020-12-11 11:41:55 -0600

    preparing for production (helmet and compression)

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const Joi = require('joi-oid');
 const express = require('express');
 const app = express();
 
-require("./startup/logging")();
-require("./startup/cors")(app);
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
+require('./startup/logging')();
+require('./startup/cors')(app);
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/prod')(app);
 
 
-const port = process.env.PORT || config.get("port");
+const port = process.env.PORT || config.get('port');
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
