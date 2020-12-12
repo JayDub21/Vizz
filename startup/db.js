117e08e9212f1d0e6d42817e57f603babf2fed15
JayDub21 <justin.wofford.21@gmail.com>
2020-12-11 18:58:30 -0600

    for production only

--- startup/db.js
+++ startup/db.js
@@ -4,32 +4,24 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
-const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
 require('dotenv').config();
 
 
 
 module.exports = () => {
-    const db = config.get('db');
-    function switcher() {
-        if(process.env.NODE_ENV == 'production'){
-            const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
-            client.connect(err => {
-              const collection = client.db("test").collection("devices");
-              // perform actions on the collection object
-              client.close();
-            });
-        }
-        else{
-            return db;
-        }
-    };
-    mongoose.connect(switcher())
-    .then(() => winston.info(`Connected to ${switcher()}...`));
+    // const db = config.get('db');
+    // function switcher() {
+    //     if(process.env.NODE_ENV == 'production'){
+    //         return process.env.MONGODB_URI
+    //     }
+    //     else{
+    //         return db;
+    //     }
+    // };
+    mongoose.connect(MONGODB_URI);
+    // .then(() => winston.info(`Connected to ${switcher()}...`));
 }
-
-
 
 
 
