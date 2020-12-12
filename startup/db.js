cf4a9ee599d769a8542a83a9ddd924b22050fa33
JayDub21 <justin.wofford.21@gmail.com>
2020-12-11 18:44:30 -0600

    atlas connection string

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
+const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
 require('dotenv').config();
 
@@ -19,7 +20,12 @@ module.exports = () => {
     const db = config.get('db');
     function switcher() {
         if(process.env.NODE_ENV == 'production'){
-            return process.env.MONGODB_URI
+            const client = new MongoClient(MONGOOSE_URI, { useNewUrlParser: true });
+            client.connect(err => {
+              const collection = client.db("test").collection("devices");
+              // perform actions on the collection object
+              client.close();
+            });
         }
         else{
             return db;
@@ -28,3 +34,9 @@ module.exports = () => {
     mongoose.connect(switcher())
     .then(() => winston.info(`Connected to ${switcher()}...`));
 }
+
+
+
+
+
+
